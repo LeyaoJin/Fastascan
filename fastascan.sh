c7deab28220c7bc7a35d970baed61562d8f586b8
LeyaoJin <leyao.jin23@gmail.com>
2024-12-03 22:48:27 +0100

    Final commit

--- fastascan.sh
+++ fastascan.sh
@@ -18,7 +18,7 @@ all_ids=""
 # Process files in the given folder (and subfolders)
 for file in $(find "$folder" -type f -name "*.fasta" -o -name "*.fa"); do
 
-    #Validate fasta
+    #Validate fasta file
     if ! [[ -s "$file" ]]; then
         echo "Error: $file is an empty FASTA file. Skipping..."
         echo "-------------------------------"
@@ -27,7 +27,8 @@ for file in $(find "$folder" -type f -name "*.fasta" -o -name "*.fa"); do
         echo "Error: $file is an invalid FASTA file due to incorrect format or missing headers. Skipping..."
         echo "-------------------------------"
         continue
-    elif ! [[ $(grep -v '^>' "$file" | grep -i '^[ACGTUNRDEQHILKMFPSWYXV-]*$') ]]; then
+    elif [[ $(grep -v '^>' "$file" | grep -i -v '^[ACGTUNRDEQHILKMFPSWYXV-]*$') ]]; then
+# Translation: If there are any characters after greping out the headers and the letters that matches with sequences of amino acis and nucleotides, then
         echo "Error: $file is an invalid FASTA file without sequences or the sequences contain invalid characters. Skipping..."
         echo "-------------------------------"
         continue
@@ -36,26 +37,28 @@ for file in $(find "$folder" -type f -name "*.fasta" -o -name "*.fa"); do
     #Total files counter
     total_files=$((total_files + 1))
 
-    # Fasta IDs
+    # Fasta IDs counter
     all_ids+=$(awk '/^>/{print $1}' "$file" | sort | uniq)
 
     # Symlink
-    if [ -h "$file" ]; then
+    if [[ -h "$file" ]]; then
         symlink="Yes"
     else
         symlink="No"
     fi
 
-    # Varaible to store the content of the file without gaps, spaces and new line characters
+    # Variable to store the content of the file without gaps, spaces and new line characters
     cleaned_file=$(awk '/^[^>]/ {gsub(/[-\t\r\v\f]/, "", $0); ORS=""; print $0}' "$file")
 
-    # Number of sequences en total length
+    # Number of sequences
     num_sequences=$(grep -c '^>' "$file")
+
+    # Total length
     total_length=$(echo -n "$cleaned_file" | wc -c)
 
 # Comment!: After storaging the cleaned file in the variable,
 # when it does echo it adds a new line or something that counts as another character,
-# I tried to use sed with [:space:] and also with [-\t\r\v\n\f] but it didn't work, so I that is why I used echo -n
+# It was tried to solve that using sed with [:space:] and also with [-\t\r\v\n\f] but it didn't work, so that is why echo -n is used
 
     # To determine if it's nucleotide or amino acid
     if  [[ $(echo -n "$cleaned_file" | grep -i '^[ACGTUN-]*$') ]]; then
@@ -86,7 +89,7 @@ for file in $(find "$folder" -type f -name "*.fasta" -o -name "*.fa"); do
 done
 
 # Comment!: all_ids appends an empty string when no IDs are found,
-# it gives a wrong value of 1 when the empty string passes through sort | uniq | wc -l, so I made this:
+# it gives a wrong value of 1 when the empty string passes through sort | uniq | wc -l, to solve that this was made:
 # Total unique IDs
 if [[ -n "$all_ids" ]]; then
     total_unique_ids=$(echo "$all_ids" | sort | uniq | wc -l)
